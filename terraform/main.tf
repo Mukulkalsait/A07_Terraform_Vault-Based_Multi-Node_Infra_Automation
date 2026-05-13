b52ba6dbd71e16cc4af67b6a54ecb69e85196607
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2026-05-13 19:21:54 +0530

    Updates

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,4 +7,22 @@ terraform{
   }
 }
 
-provider "lxd" {}
+provider "lxd" {
+  accept_remote_certificate = true
+
+; each remote = 1 host
+  remote{
+    name = "ubuntu-server"
+    address = "https://192.168.1.2:8443"
+    default = true
+    token = var.ubuntu_lxd_token
+  }
+
+  remote{
+    name = "zorin-host"
+    address = "https://192.168.1.3:8443"
+    token = var.zorin_lxc_token
+  }
+
+  
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+
+  variable "ubuntu_lxd_token"{
+    description = "LXD of ubuntu-server"
+    type = string
+    sensitive = true
+  }
+
+  variable "zorin_lxc_token"{
+    description = "LXD of zorin"
+    type = string
+    sensitive = true
+  }
